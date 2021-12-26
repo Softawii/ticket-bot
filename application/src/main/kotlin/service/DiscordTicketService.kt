e0d14016bf14545d472c13df23db5d914cfbe7f2
FerroEduardo <47820549+ferroeduardo@users.noreply.github.com>
2021-12-26 19:13:29 -0300

    refactor(DiscordTicketService): implementation of some missing functions #18

--- application/src/main/kotlin/service/DiscordTicketService.kt
+++ application/src/main/kotlin/service/DiscordTicketService.kt
@@ -11,45 +11,54 @@ object DiscordTicketService:TicketService {
     private val ticketRepository = TicketRepository.getInstance()
 
     override fun archiveTicket(platformId: Long, ticketId: Long): String {
-//        val ticketOptional = database.findTicketById(ticketId)
-//        if (ticketOptional.isEmpty || (ticketOptional.get().client!!.discordId != platformId)) {
-//            return "Esse ticket não existe ou não é seu ticket"
-//        } else {
-//            return "Ticket de ID ${ticketOptional.get().id} arquivado com sucesso"
-//        }
-        TODO()
+        val ticketOptional = ticketRepository.findById(ticketId)
+        if (ticketOptional.isEmpty || (ticketOptional.get().client!!.discordId != platformId)) {
+            return "Esse ticket não existe ou não é seu ticket"
+        } else {
+            val ticket = ticketOptional.get()
+            ticket.isArchived = true
+            ticketRepository.update(ticket)
+            if (ticket.client!!.activeTicket != null &&
+                ticket.client!!.activeTicket!!.id == ticket.id) {
+                val client = ticket.client!!
+                client.activeTicket = null
+                clientRepository.update(ticket.client!!)
+            }
+            return "Ticket de ID ${ticketOptional.get().id} arquivado com sucesso"
+        }
     }
 
     override fun archiveCurrentTicket(platformId: Long): String {
-//        val clientOptional = database.findClientByDiscordId(platformId)
-//        if (clientOptional.isEmpty) {
-//            return "Você ainda não está cadastrado. Crie um ticket antes"
-//        } else {
-//            val client = clientOptional.get()
-//            if (client.activeTicket == null) {
-//                return "Você não tem ticket ativo para poder ser arquivado"
-//            } else {
-//                val ticket = client.activeTicket!!
-//                client.activeTicket = null
-//                return "Ticket ativo (${ticket.id}) arquivado com sucesso"
-//            }
-//        }
-        TODO()
+        val clientOptional = clientRepository.findByDiscordId(platformId)
+        if (clientOptional.isEmpty) {
+            return "Você ainda não está cadastrado. Crie um ticket antes"
+        } else {
+            val client = clientOptional.get()
+            if (client.activeTicket == null) {
+                return "Você não tem ticket ativo para poder ser arquivado"
+            } else {
+                val ticket = client.activeTicket!!
+                client.activeTicket = null
+                ticket.isArchived = true
+                ticketRepository.update(ticket)
+                clientRepository.update(client)
+                return "Ticket (${ticket.id}) arquivado com sucesso"
+            }
+        }
     }
 
     override fun activeTicket(platformId: Long): String {
-//        val clientOptional = database.findClientByDiscordId(platformId)
-//        if (clientOptional.isEmpty) {
-//            return "Você ainda não está cadastrado. Crie um ticket antes"
-//        } else {
-//            val client = clientOptional.get()
-//            return if (client.activeTicket == null) {
-//                "Você não tem tickets ativos"
-//            } else {
-//                "Seu ticket ativo é o de ID ${client.activeTicket!!.id}"
-//            }
-//        }
-        TODO()
+        val clientOptional = clientRepository.findByDiscordId(platformId)
+        if (clientOptional.isEmpty) {
+            return "Você ainda não está cadastrado. Crie um ticket antes"
+        } else {
+            val client = clientOptional.get()
+            if (client.activeTicket == null) {
+                return "Você não tem tickets ativos"
+            } else {
+                return "Seu ticket ativo é o de ID ${client.activeTicket!!.id}"
+            }
+        }
     }
 
     override fun createTicket(platformId: Long): String {
