f7c6f45a34f2d198c75adbe9b1e22c9e24c0ddf5
FerroEduardo <47820549+ferroeduardo@users.noreply.github.com>
2022-01-04 16:48:24 -0300

    refactor: new method to add entity classes

--- application/src/main/kotlin/com/softawii/ticketbot/util/HibernateUtil.kt
+++ application/src/main/kotlin/com/softawii/ticketbot/util/HibernateUtil.kt
@@ -1,8 +1,5 @@
 package com.softawii.ticketbot.util
 
-import com.softawii.ticketbot.entity.Client
-import com.softawii.ticketbot.entity.Message
-import com.softawii.ticketbot.entity.Ticket
 import org.hibernate.SessionFactory
 import org.hibernate.boot.registry.StandardServiceRegistry
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder
@@ -16,10 +13,12 @@ class HibernateUtil private constructor() {
         private fun init(): HibernateUtil {
             val configuration = Configuration()
             configuration.configure()
-                .addAnnotatedClass(Client::class.java)
-                .addAnnotatedClass(Message::class.java)
-                .addAnnotatedClass(Ticket::class.java)
                 .addProperties(PropertiesUtil.properties)
+
+            val commandClasses = ReflectionUtil.getClassesByPackageName("com.softawii.ticketbot.entity")
+            commandClasses.forEach {
+                configuration.addAnnotatedClass(Class.forName(it.name))
+            }
             serviceRegistry = StandardServiceRegistryBuilder().applySettings(configuration.properties).build()
             sessionFactory = configuration.buildSessionFactory(serviceRegistry)
             return HibernateUtil()
