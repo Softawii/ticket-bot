2ead45dd5dfba6ca568c1ff852d00ab4456e215b
FerroEduardo <47820549+ferroeduardo@users.noreply.github.com>
2022-01-06 10:14:35 -0300

    feat: Setup servers method

--- application/src/main/kotlin/com/softawii/ticketbot/service/DiscordTicketService.kt
+++ application/src/main/kotlin/com/softawii/ticketbot/service/DiscordTicketService.kt
@@ -1,6 +1,7 @@
 package com.softawii.ticketbot.service
 
 import com.softawii.ticketbot.entity.Client
+import com.softawii.ticketbot.entity.DiscordServer
 import com.softawii.ticketbot.entity.Ticket
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.Permission
@@ -11,6 +12,7 @@ object DiscordTicketService: TicketService {
     private val LOGGER = LogManager.getLogger(DiscordTicketService.javaClass)
     private val clientRepository = ClientRepository.getInstance()
     private val ticketRepository = TicketRepository.getInstance()
+    private val discordServerRepository = DiscordServerRepository.getInstance()
 
     @JvmStatic
     fun getInviteLink(jda: JDA): String {
@@ -74,7 +76,7 @@ object DiscordTicketService: TicketService {
         }
     }
 
-    override fun createTicket(platformId: Long): String {
+    override fun createTicket(platformId: Long, serverId: Long): String {
         val clientOptional = clientRepository.findByDiscordId(platformId)
         val client: Client
         if (clientOptional.isEmpty) {
@@ -85,10 +87,28 @@ object DiscordTicketService: TicketService {
             client = clientOptional.get()
         }
 
-        val ticket = ticketRepository.save(Ticket(null, client))
+        val serverOptional = discordServerRepository.findByServerId(serverId)
+        if (serverOptional.isEmpty || serverOptional.get().categoryId == null) {
+            return "O servidor ainda não foi configurado corretamente"
+        }
+        val ticket = ticketRepository.save(Ticket(id = null, client = client, discordServer = serverOptional.get()))
         client.activeTicket = ticket
         clientRepository.update(client)
 
         return "O seu novo ticket tem o ID: ${ticket.id}. Para enviar alguma mensagem para esse ticket utilize `/send-message ${ticket.id} MENSAGEM`"
+    }
+
+    override fun setupServer(serverId: Long): String {
+        val discordServerOptional = discordServerRepository.findByServerId(serverId)
+        if (discordServerOptional.isPresent) {
+            if (discordServerOptional.get().categoryId == null) {
+                return "O servidor já está em nossa base de dados mas ainda falta configurar a categoria do servidor"
+            }
+            return "O servidor já foi configurado anteriormente"
+        }
+        val discordServer = DiscordServer(null, serverId, null)
+        discordServerRepository.save(discordServer)
+
+        return "Servidor configurado com sucesso. Defina uma categoria para que os tickets possam ser criados"
     }
 }
--- application/src/main/kotlin/com/softawii/ticketbot/service/TicketService.kt
+++ application/src/main/kotlin/com/softawii/ticketbot/service/TicketService.kt
@@ -4,5 +4,6 @@ interface TicketService {
     fun archiveTicket(platformId: Long, ticketId: Long): String
     fun archiveCurrentTicket(platformId: Long): String
     fun activeTicket(platformId: Long): String
-    fun createTicket(platformId: Long): String
+    fun createTicket(platformId: Long, serverId:Long): String
+    fun setupServer(serverId: Long): String
 }
