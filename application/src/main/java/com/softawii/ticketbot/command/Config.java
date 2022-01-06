67dd797ba564261051028c7eb3d503d6c07ad5e7
Yan Figueiredo <yancfm@gmail.com>
2022-01-06 11:16:09 -0300

    feat: set basic configuration #25

--- application/src/main/java/com/softawii/ticketbot/command/Config.java
+++ application/src/main/java/com/softawii/ticketbot/command/Config.java
@@ -3,6 +3,7 @@ package com.softawii.ticketbot.command;
 import com.softawii.ticketbot.annotation.Argument;
 import com.softawii.ticketbot.annotation.Command;
 import com.softawii.ticketbot.annotation.CommandClass;
+import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
@@ -13,13 +14,19 @@ public class Config {
     @Argument(type= OptionType.BOOLEAN, name="support", description = "Set Current Chat as Support", isRequired = true)
     public static String set(SlashCommandEvent event) {
 
-        System.out.println("In Config.set");
-
         boolean supportChat = event.getOption("support").getAsBoolean();
 
-        System.out.println("supportChat: " + supportChat);
+        Long guildId = event.getGuild().getIdLong();
 
-        if(supportChat) event.reply("Que papinho hein\n").queue();
+        Category category = event.getTextChannel().getParentCategory();
+
+        if(category == null) {
+            return "This channel is not in a category";
+        }
+
+        String content = category.getIdLong() + " : " + guildId;
+
+        if(supportChat) event.reply("Que papinho hein" + content + "\n").queue();
         else event.reply(" sem papinho").queue();
 
         return null;
